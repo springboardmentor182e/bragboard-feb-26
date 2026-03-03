5f3f5605b0cb693e68b1a62374b80706dfec4e6c
Thammisetty Jeevan <thammisettyjeevan@gmail.com>
2026-03-03 10:35:31 +0530

    Completed Admin Shoutout Management with backend integration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import './index.css'
+import AdminDashboard from './adminShoutout/AdminDashboard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+    <AdminDashboard></AdminDashboard>
+  </React.StrictMode>,
 )
