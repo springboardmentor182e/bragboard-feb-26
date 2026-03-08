c90f950a5873c38211a343c1bff077e4fddb621b
yashnavi <yashnavinarasimman@gmail.com>
2026-03-08 15:14:58 +0530

    app.jsx completed

---
+++ client/src/Apps.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
+function App(){
+
+return(
+
+<BrowserRouter>
+
+<Routes>
+
+<Route path="/" element={<Register/>}/>
+<Route path="/login" element={<Login/>}/>
+
+</Routes>
+
+</BrowserRouter>
+
+)
+
+}
+
+export default App
