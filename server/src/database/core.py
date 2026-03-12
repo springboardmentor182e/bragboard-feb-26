8981d52c3cf7cb4cf088ed0628cb34d993f053bc
Sterling Richie <sterlingrichie46@gmail.com>
2026-03-12 19:44:27 +0530

    Resolved merge conflicts

--- server/src/database/core.py
+++ server/src/database/core.py
@@ -1,16 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:richie@localhost:5432/bragboard"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(
-    bind=engine,
     autocommit=False,
-    autoflush=False
+    autoflush=False,
+    bind=engine
 )
-
 
 class Base(DeclarativeBase):
     pass
