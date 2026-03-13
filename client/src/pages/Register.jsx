d08ec47f66c2482baf917e0b0e900ec7681251b5
yashnavi <yashnavinarasimman@gmail.com>
2026-03-13 22:54:29 +0530

    updated register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,54 +1,58 @@
 import { useState } from "react";
 import { registerUser } from "../services/api";
 
-function Register(){
+export default function Register(){
 
 const [email,setEmail] = useState("");
 const [password,setPassword] = useState("");
 
-const handleSubmit = async (e) => {
+const handleSubmit = async(e)=>{
 e.preventDefault();
 
 try{
 await registerUser({email,password});
-alert("User Registered");
+alert("Register Success");
 }
-catch(err){
-alert("Error");
+catch{
+alert("Register Failed");
 }
-};
+
+}
 
 return(
 
-<div className="flex items-center justify-center h-screen bg-gray-100">
+<div className="flex items-center justify-center min-h-screen bg-gray-100">
 
-<form onSubmit={handleSubmit} className="bg-white p-8 shadow-md rounded w-96">
+<form onSubmit={handleSubmit} className="bg-white p-8 rounded-xl shadow-lg w-96">
 
-<h2 className="text-2xl font-bold mb-4">Register</h2>
+<h2 className="text-2xl font-bold mb-6 text-center">
+Register
+</h2>
 
 <input
 type="email"
 placeholder="Email"
-className="border p-2 w-full mb-3"
+className="w-full p-3 border rounded mb-4"
 onChange={(e)=>setEmail(e.target.value)}
 />
 
 <input
 type="password"
 placeholder="Password"
-className="border p-2 w-full mb-3"
+className="w-full p-3 border rounded mb-4"
 onChange={(e)=>setPassword(e.target.value)}
 />
 
-<button className="bg-green-500 text-white p-2 w-full">
+<button className="w-full bg-green-500 text-white p-3 rounded hover:bg-green-600">
+
 Register
+
 </button>
 
 </form>
 
 </div>
 
-);
-}
+)
 
-export default Register;
+}
