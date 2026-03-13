a982e32127042608177a0c46e3b1d5c6ef24debc
yashnavi <yashnavinarasimman@gmail.com>
2026-03-13 22:48:41 +0530

    login page completed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,53 +1,59 @@
 import { useState } from "react";
 import { loginUser } from "../services/api";
 
-function Login() {
+export default function Login() {
 
-const [email,setEmail] = useState("");
-const [password,setPassword] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
 
-const handleSubmit = async (e) => {
-e.preventDefault();
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
 
-try{
-const res = await loginUser({email,password});
-localStorage.setItem("token",res.data.access_token);
-alert("Login successful");
-}
-catch(err){
-alert("Login failed");
-}
-};
+    try{
+      const res = await loginUser({email,password});
+      console.log(res.data);
+      alert("Login Success");
+    }
+    catch(err){
+      alert("Login Failed");
+    }
+  }
 
-return(
-<div className="flex items-center justify-center h-screen bg-gray-100">
+  return(
 
-<form onSubmit={handleSubmit} className="bg-white p-8 shadow-md rounded w-96">
+<div className="flex items-center justify-center min-h-screen bg-gray-100">
 
-<h2 className="text-2xl font-bold mb-4">Login</h2>
+<form onSubmit={handleSubmit} className="bg-white p-8 rounded-xl shadow-lg w-96">
+
+<h2 className="text-2xl font-bold mb-6 text-center">
+Login
+</h2>
 
 <input
 type="email"
 placeholder="Email"
-className="border p-2 w-full mb-3"
+className="w-full p-3 border rounded mb-4"
+value={email}
 onChange={(e)=>setEmail(e.target.value)}
 />
 
 <input
 type="password"
 placeholder="Password"
-className="border p-2 w-full mb-3"
+className="w-full p-3 border rounded mb-4"
+value={password}
 onChange={(e)=>setPassword(e.target.value)}
 />
 
-<button className="bg-blue-500 text-white p-2 w-full">
+<button className="w-full bg-blue-500 text-white p-3 rounded hover:bg-blue-600">
+
 Login
+
 </button>
 
 </form>
 
 </div>
-);
-}
 
-export default Login;
+  );
+}
