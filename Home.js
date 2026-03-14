908f298a34b0ee76162fde06f59ba8d44d120098
Abarna-1606 <abarna1232007@gmail.com>
2026-03-14 21:31:16 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Leaderboard from "../components/Leaderboard";
+
+function Home() {
+  return (
+    <div>
+      <h1>Game Leaderboard</h1>
+      <Leaderboard />
+    </div>
+  );
+}
+
+export default Home;
