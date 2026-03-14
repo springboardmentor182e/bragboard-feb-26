89ccada89e85d0af82f9b1312b2b269f9fda77a6
yashnavi <yashnavinarasimman@gmail.com>
2026-03-15 00:40:54 +0530

    updated

--- server/src/main.py
+++ server/src/main.py
@@ -5,34 +5,35 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# CORS FIX
+origins = [
+    "http://localhost:5173",
+    "http://localhost:5174",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 class User(BaseModel):
-
-    email:str
-    password:str
+    email: str
+    password: str
 
 
 @app.post("/register")
-
-def register(user:User):
-
-    return {"message":"User Registered"}
+def register(user: User):
+    return {"message": "User Registered"}
 
 
 @app.post("/login")
-
-def login(user:User):
-
-    token = create_access_token({"sub":user.email})
+def login(user: User):
+    token = create_access_token({"sub": user.email})
 
     return {
-        "access_token":token,
-        "token_type":"bearer"
+        "access_token": token,
+        "token_type": "bearer"
     }
