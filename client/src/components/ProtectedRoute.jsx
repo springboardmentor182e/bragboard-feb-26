a9cdc306bf828f1c111f7e11f00f874417fce0ce
yashnavi <yashnavinarasimman@gmail.com>
2026-03-14 19:14:06 +0530

    sucessfully setup route

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,12 +2,12 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({children}){
 
-const token = localStorage.getItem("token")
+const token = localStorage.getItem("token");
 
 if(!token){
 return <Navigate to="/" />
 }
 
-return children
+return children;
 
 }
