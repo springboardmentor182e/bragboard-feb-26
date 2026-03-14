15943a6a161f97f3c5a09aa142f7756fde3a227e
yashnavi <yashnavinarasimman@gmail.com>
2026-03-14 16:13:06 +0530

    completed api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
-});
+ baseURL:"http://127.0.0.1:8000"
+})
 
-export const registerUser = (data) => API.post("/register", data);
-export const loginUser = (data) => API.post("/login", data);
-
-export default API;  
+export const loginUser=(data)=>API.post("/login",data)
+export const registerUser=(data)=>API.post("/register",data)
