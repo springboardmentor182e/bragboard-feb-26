9d652421a13282cba4b173de255480646961700e
Akshaya-J-19222 <akshayajeyakumar09@gmail.com>
2026-03-16 12:13:39 +0530

    Removed code

--- client/src/features/authentication/services/login.js
+++
@@ -1,6 +0,0 @@
-export const loginUser = async (email, password) => {
-  console.log("Logging in with:", email, password);
-
-  // later we will connect this to backend
-  return { success: true };
-};
