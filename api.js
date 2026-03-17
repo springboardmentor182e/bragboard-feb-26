27b912eb9fc0742d698b6c29b06b112ffb49c1b1
Abarna-1606 <abarna1232007@gmail.com>
2026-03-17 20:12:48 +0530

    Delete api.js

--- api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
-});
-export const getLeaderboard = () => API.get("/leaderboard");
-export const addPlayer = (player) => API.post("/leaderboard", player);
