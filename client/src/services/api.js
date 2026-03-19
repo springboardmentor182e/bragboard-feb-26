18788a696d6815e133762a6473ea249fe1179cd0
yashnavi <yashnavinarasimman@gmail.com>
2026-03-19 19:38:24 +0530

    updated successfully

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "http://127.0.0.1:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Attach token automatically
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
 });
 
 export const loginUser = (data) => API.post("/login", data);
