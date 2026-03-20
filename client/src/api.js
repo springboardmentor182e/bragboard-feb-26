c66097546e01b0299e36448d28bcc29d3a2cbb0d
mannuchouhan2915 <mannuchouhan2915@gmail.com>
2026-03-20 15:07:38 +0530

    changes in api.js ,.env, .gitignore

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://bragboard-feb-26.onrender.com/";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
 export const getShoutouts = () => {
   return axios.get(API_URL);
