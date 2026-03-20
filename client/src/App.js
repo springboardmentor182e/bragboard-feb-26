d8e66544135439bdde5177214bf8a43970c4b057
Abarna-1606 <abarna1232007@gmail.com>
2026-03-20 10:55:33 +0530

    Implement routing with BrowserRouter and Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EmployeeDashboard from "./features/employeeDashboard/EmployeeDashboard";
 import React from "react";
 import Home from "./pages/Home";
+import Leaderboard from "./pages/Leaderboard"; // if you have this
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/employee/*" element={<EmployeeDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
