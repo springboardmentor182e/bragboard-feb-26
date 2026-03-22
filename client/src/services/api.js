5ce3aa4a31e252df68224b180f81ce8910bba3bd
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 17:32:51 +0530

    changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,38 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://127.0.0.1:8000",
 });
 
-// attach token
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-// refresh token
-API.interceptors.response.use(
-  (res) => res,
-  async (err) => {
-    if (err.response?.status === 401) {
-      const refresh = localStorage.getItem("refresh_token");
-
-      const res = await axios.post("http://localhost:8000/refresh", {
-        token: refresh,
-      });
-
-      localStorage.setItem("access_token", res.data.access_token);
-
-      err.config.headers.Authorization = `Bearer ${res.data.access_token}`;
-      return API(err.config);
-    }
-  }
-);
-
-// APIs
 export const loginUser = (data) => API.post("/login", data);
-export const createShoutout = (data) => API.post("/shoutout", data);
+export const registerUser = (data) => API.post("/register", data);
 export const getShoutouts = () => API.get("/shoutouts");
+export const createShoutout = (data) => API.post("/shoutout", data);
 
 export default API;
