adaaa5d159b490ba6ed5d01a132bc4855265a66d
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 15:32:48 +0530

    changed

---
+++ server/src/core/auth.py
@@ -0,0 +1,13 @@
+from jose import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "secret"
+ALGORITHM = "HS256"
+
+def create_access_token(data: dict):
+    data.update({"exp": datetime.utcnow() + timedelta(minutes=30)})
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+def create_refresh_token(data: dict):
+    data.update({"exp": datetime.utcnow() + timedelta(days=7)})
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
