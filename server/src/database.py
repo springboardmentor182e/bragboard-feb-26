3405553d192b1473f468f1b6313d2abe752522da
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 12:54:35 +0530

    changes done

--- server/src/database.py
+++ server/src/database.py
@@ -1,22 +1,10 @@
-# database.py
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:password@localhost/shoutout_db"
+DATABASE_URL = "postgresql://postgres:password@localhost/bragboard"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
+
 Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String)
-    username = Column(String, unique=True)
-    email = Column(String, unique=True, index=True)
-    department = Column(String)
-    job_title = Column(String)
-    password = Column(String)
-
-Base.metadata.create_all(bind=engine)
