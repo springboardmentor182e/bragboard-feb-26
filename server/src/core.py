0a48288578fdf382d841553ee4821e99d0f8be49
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 16:43:12 +0530

    done

--- server/src/core.py
+++ server/src/core.py
@@ -1,11 +1,13 @@
-import psycopg2
+from jose import jwt
+from datetime import datetime, timedelta
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="bragboard_db",
-    user="postgres",
-    password="reshma894",
-    port="5432"
-)
+SECRET_KEY = "secret"
+ALGORITHM = "HS256"
 
-cursor = conn.cursor()
+def create_access_token(data: dict):
+    data.update({"exp": datetime.utcnow() + timedelta(minutes=30)})
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+def create_refresh_token(data: dict):
+    data.update({"exp": datetime.utcnow() + timedelta(days=7)})
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
