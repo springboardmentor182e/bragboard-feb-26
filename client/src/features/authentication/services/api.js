a8f82746fa46001815be78974dea3a35a35e3fc3
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 12:19:53 +0530

    corrected

--- client/src/features/authentication/services/api.js
+++ client/src/features/authentication/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "http://localhost:8000",
 });
-
-export const loginUser = (data) => API.post("/login", data);
-export const registerUser = (data) => API.post("/register", data);
 
 export default API;
