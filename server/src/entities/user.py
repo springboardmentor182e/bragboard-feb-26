107fb80448aebc967b10fd3fc5f40d8d372ca558
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 19:02:05 +0530

    Removed node_modules and frontend from repo

--- server/src/entities/user.py
+++ server/src/entities/user.py
@@ -1,22 +1,16 @@
 from sqlalchemy import Column, Integer, String
-<<<<<<< HEAD
 from database.db import Base
-=======
 from src.database.core import Base
->>>>>>> b965fc4a7c5ebee34091241f01986483a1400b8f
 
 class User(Base):
 
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-<<<<<<< HEAD
     email = Column(String, unique=True)
     password = Column(String)
-=======
     name = Column(String)
     department = Column(String)
     points = Column(Integer)
     badges = Column(Integer)
     trend = Column(String)
->>>>>>> b965fc4a7c5ebee34091241f01986483a1400b8f
--- server/src/models.py
+++ server/src/models.py
@@ -1,11 +1,1 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String)
-    username = Column(String, unique=True)
-    email = Column(String, unique=True)
-    password = Column(String)
