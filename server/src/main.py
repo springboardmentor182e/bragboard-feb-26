ee59ca97f9fe0e4be352019e19022ce585b80efe
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 16:15:00 +0530

    changed

--- server/src/main.py
+++ server/src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.routes import auth
-from app.database import Base, engine
+from src.routes import auth
+from src.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
