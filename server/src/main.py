37f83d0582718836164a5ee43d02b1314304c09f
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 16:50:48 +0530

    changend

--- server/src/main.py
+++ server/src/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from src.routes import auth
 from src.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(auth.router)
