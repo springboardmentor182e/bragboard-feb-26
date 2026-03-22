2323a2a2c8076064b64071e56cbf9272179ff65b
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 13:38:19 +0530

    completed

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,13 +1,7 @@
+// src/components/ProtectedRoute.jsx
 import { Navigate } from "react-router-dom";
+import { isAuthenticated } from "../utils/auth";
 
-export default function ProtectedRoute({children}){
-
-const token = localStorage.getItem("token");
-
-if(!token){
-return <Navigate to="/" />
-}
-
-return children;
-
+export default function ProtectedRoute({ children }) {
+  return isAuthenticated() ? children : <Navigate to="/" />;
 }
