c0804e50624701c48b6e1f250def2260c44a93b6
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 16:24:19 +0530

    changed

--- server/src/database.py
+++ server/src/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://yashnavi:123456@localhost/bragboard"
+DATABASE_URL = "postgresql://postgres:password@localhost/bragboard"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
