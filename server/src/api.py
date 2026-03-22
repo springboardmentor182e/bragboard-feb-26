0aa1466a4a80d302354c1fe6ff58187db5e8e8f1
Thammisetty Jeevan <thammisettyjeevan@gmail.com>
2026-03-22 22:55:34 +0530

    Admin Shoutout Management Feature

--- server/src/api.py
+++ server/src/api.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from src.users.controller import router as users_router
 from src.auth.controller import router as auth_router
 from src.reports.controller import router as reports_router
+from src.shoutouts.controller import router as shoutouts_router
 
 router = APIRouter()
 
@@ -14,3 +15,5 @@ router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 
 # Reports APIs
 router.include_router(reports_router, prefix="/reports", tags=["Reports"])
+
+router.include_router(shoutouts_router)
