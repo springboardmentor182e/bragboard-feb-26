10001b2a8fe8556839ea20444de5e0306f133f72
yashnavi <yashnavinarasimman@gmail.com>
2026-03-22 13:23:31 +0530

    changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,4 +4,5 @@ const API = axios.create({
   baseURL: "http://localhost:8000",
 });
 
-export default API;
+export const loginUser = (data) => API.post("/login", data);
+export const registerUser = (data) => API.post("/register", data);
