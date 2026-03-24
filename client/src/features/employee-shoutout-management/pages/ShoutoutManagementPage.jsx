dbd14872f531746fc64bdcf1697f009ef591ab45
satwik146 <mnsatwik146.gmail>
2026-03-24 18:54:40 +0530

    resolving merge conflicts

--- client/src/features/employee-shoutout-management/pages/ShoutoutManagementPage.jsx
+++ client/src/features/employee-shoutout-management/pages/ShoutoutManagementPage.jsx
@@ -5,7 +5,7 @@ import ShoutoutTable from "../components/ShoutoutTable";
 
 const ShoutoutManagementPage = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-50/50 to-white px-6 py-8 w-full">
+    <div className="min-h-screen bg-gradient-to-br from-purple-50/50 to-white p-8 space-y-8">
       <ShoutoutTable />
     </div>
   );
