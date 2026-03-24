b064feafe02e37e576ae5d85ab6ce1223beea6b1
Thammisetty Jeevan <thammisettyjeevan@gmail.com>
2026-03-24 23:37:21 +0530

    Updated Admin Shoutout (env fix + UI + API integration)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,22 +5,17 @@ import TopNavbar from "./features/employeeDashboard/components/TopNavbar";
 import SummaryCards from "./features/employeeDashboard/components/SummaryCards";
 import AchievementTable from "./features/employeeDashboard/components/AchievementTable";
 import Leaderboard from "./features/employeeDashboard/components/Leaderboard";
-import AdminDashboard from "./features/admin-dash/pages/AdminDashboard.jsx";
 
 import AdminEmployees from "./pages/AdminEmployees";
 import AdminReports from "./pages/AdminReports";
-import AdminDashboard from "./features/Adminshoutout/AdminDashboard";
-import AdminSidebar from "./layout/AdminSidebar.jsx";
-import AdminTopbar from "./layout/AdminTopbar.jsx";
+import AdminShoutout from "./features/Adminshoutout/AdminShoutout";
 
 function EmployeeLayout({ children }) {
   return (
     <div className="flex h-screen bg-gray-100">
       <Sidebar />
-
       <div className="flex-1 flex flex-col">
         <TopNavbar />
-
         <main className="flex-1 p-6 overflow-y-auto">
           {children}
         </main>
@@ -38,27 +33,11 @@ function EmployeeDashboard() {
     </>
   );
 }
-function AdminLayout({ children }) {
-  return (
-    <div className="bg-[#EEF2F7] min-h-screen">
-      <div className="fixed top-0 left-0 h-full w-72 z-50">
-        <AdminSidebar />
-      </div>
-      <div className="ml-72 min-h-screen">
-        <AdminTopbar />
-        <main className="p-6">
-          {children}
-        </main>
-      </div>
-    </div>
-  );
-}
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* Employee Dashboard */}
         <Route
           path="/"
           element={
@@ -67,19 +46,12 @@ function App() {
             </EmployeeLayout>
           }
         />
-{/* Admin Routes - Flat */}
-          <Route path="/admin" element={
-            <AdminLayout>
-              <AdminDashboard />
-            </AdminLayout>
-          } />
-        {/* Admin Employee Management */}
+
         <Route
           path="/admin/employees"
           element={<AdminEmployees />}
         />
 
-        {/* Admin Reports */}
         <Route
           path="/admin/reports"
           element={<AdminReports />}
@@ -89,11 +61,10 @@ function App() {
           path="/admin-dashboard"
           element={
             <EmployeeLayout>
-              <AdminDashboard />
+              <AdminShoutout />
             </EmployeeLayout>
           }
         />
-
       </Routes>
     </BrowserRouter>
   );
