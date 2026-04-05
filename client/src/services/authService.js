de52cbd4f3576f498ee9aa1a392e22bad8f0f15a
AadityaGeek <aadityakumar1024+github@gmail.com>
2026-04-05 12:36:14 +0530

    Deployment Changes

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API = import.meta.env.VITE_API_BASE_URL;
 
 /* LOGIN */
 export const loginUser = async (data) => {
